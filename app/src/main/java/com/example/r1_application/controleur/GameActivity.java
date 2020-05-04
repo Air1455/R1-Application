cf1565f3e2d14d1e576f810f94376e9d9adfd4ab
Air1455 <erwanngautier1@email.com>
2020-05-04 19:06:33 +0000

    Initialisation de GameActivity et Question.java

--- app/src/main/java/com/example/r1_application/controleur/GameActivity.java
+++ app/src/main/java/com/example/r1_application/controleur/GameActivity.java
@@ -3,14 +3,28 @@ package com.example.r1_application.controleur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.r1_application.R;
 
 public class GameActivity extends AppCompatActivity {
+    private TextView gameQuestion;
+    private Button gameAnswer1;
+    private Button gameAnswer2;
+    private Button gameAnswer3;
+    private Button gameAnswer4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_game );
+
+        gameQuestion = findViewById ( R.id.gameQuestion );
+        gameAnswer1 = findViewById ( R.id.gameAnswer1 );
+        gameAnswer2 = findViewById ( R.id.gameAnswer2 );
+        gameAnswer3 = findViewById ( R.id.gameAnswer3 );
+        gameAnswer4 = findViewById ( R.id.gameAnswer4 );
     }
 }
--- app/src/main/java/com/example/r1_application/model/Question.java
+++ app/src/main/java/com/example/r1_application/model/Question.java
@@ -1,4 +1,39 @@
 package com.example.r1_application.model;
 
+import java.util.List;
+
 public class Question {
+    private String question;
+    private List<String> answerChoice;
+    private int choiceId;
+
+    public Question(String question, List<String> answerChoice, int choiceId) {
+        this.question = question;
+        this.answerChoice = answerChoice;
+        this.choiceId = choiceId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getAnswerChoice() {
+        return answerChoice;
+    }
+
+    public void setAnswerChoice(List<String> answerChoice) {
+        this.answerChoice = answerChoice;
+    }
+
+    public int getChoiceId() {
+        return choiceId;
+    }
+
+    public void setChoiceId(int choiceId) {
+        this.choiceId = choiceId;
+    }
 }
