3481ce06b929a88537e7f8213a6288a30df4ef55
Air1455 <erwanngautier1@email.com>
2020-05-04 15:37:31 +0000

    Initialisation de la page mainActivity

--- app/src/main/java/com/example/r1_application/MainActivity.java
+++ app/src/main/java/com/example/r1_application/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.r1_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userNameEditText;
+    private Button firstPageGoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        firstPageGoButton = findViewById ( R.id.firstPageGoButton );
+        userNameEditText = findViewById ( R.id.userNameEditText );
+
+        firstPageGoButton.setEnabled ( false );
+        userNameEditText.addTextChangedListener ( new TextWatcher () {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(userNameEditText.length() != 0){
+                    firstPageGoButton.setEnabled ( true );
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        } );
     }
 }
