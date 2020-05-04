459178343158fd5978cfd99c8f16698655489440
Air1455 <erwanngautier1@email.com>
2020-05-04 19:22:43 +0000

    Initialisation de QuestionList.java

---
+++ app/src/main/java/com/example/r1_application/model/QuestionList.java
@@ -0,0 +1,30 @@
+package com.example.r1_application.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionList {
+    private List<Question> questionList;
+    private int nextQuestionIndex;
+
+    public QuestionList(List<Question> questionList, int nextQuestionIndex) {
+        this.questionList = questionList;
+        Collections.shuffle(questionList);
+        nextQuestionIndex = 0;
+    }
+
+    public Question getQuestion(){
+        if(nextQuestionIndex == questionList.size()){
+            nextQuestionIndex = 0;
+        }
+        return questionList.get ( nextQuestionIndex++ );
+    }
+
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(List<Question> questionList) {
+        questionList = questionList;
+    }
+}
